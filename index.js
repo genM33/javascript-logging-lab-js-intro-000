2fe56d25bace2ff81aa1e538771b6dbc103b9ba8
Geno M <giantpowpow2@gmail.com>
2019-10-16 08:32:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 console.error("HALP!");
 console.log("HALP!");
 console.warn("HALP!");
-
-const number = 20;
-if (number > 20) {
-  console.log("The number is greater than 20.")
-} else {
-  console.log("The number is less than or equal to 20.")
-}
